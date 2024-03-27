0aee1b636012f943cdad27f74ff7694950992b73
Tirt <75492176+Tirtstan@users.noreply.github.com>
2024-03-27 18:09:28 +0200

    credit and more modularity

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,67 +1,7 @@
-import Book from "./media/Book.js";
-import Utilities from "./Utilities.js";
-
-const btnAdd = document.getElementById("btnAdd");
-const btnClear = document.getElementById("btnClear");
-const btnFormat = document.getElementById("btnFormat");
 const btnCopy = document.getElementById("btnCopy");
-
-const txtAuthors = document.getElementById("txtAuthors");
-const txtAuthorsOutput = document.getElementById("txtAuthorsOutput");
-
-const txtYear = document.getElementById("txtYear");
-const txtPubPlace = document.getElementById("txtPubPlace");
-const txtBookName = document.getElementById("txtBookName");
-const txtPubName = document.getElementById("txtPubName");
-const txtEditionNum = document.getElementById("txtEditionNum");
 
 const txtOtherInfo = document.getElementById("txtOtherInfo");
 const txtOutput = document.getElementById("txtOutput");
-
-let authors = [];
-
-const date = new Date();
-txtYear.value = date.getFullYear();
-txtEditionNum.value = 1;
-
-btnAdd.addEventListener("click", () => {
-    if (txtAuthors.value === null || txtAuthors.value === "") {
-        return;
-    }
-
-    authors.push(txtAuthors.value);
-    txtAuthors.value = "";
-    txtAuthorsOutput.value = "";
-
-    for (let i = 0; i < authors.length; i++) {
-        txtAuthorsOutput.value = Utilities.listNames(authors, "and");
-    }
-});
-
-btnClear.addEventListener("click", () => {
-    authors = [];
-    txtAuthors.value = "";
-    txtAuthorsOutput.value = "";
-});
-
-btnFormat.addEventListener("click", () => {
-    if (Utilities.areNullOrEmpty(txtBookName.value, txtPubPlace.value, txtPubName.value)) {
-        return;
-    }
-
-    const book = new Book(
-        txtYear.value,
-        txtBookName.value,
-        txtEditionNum.value,
-        txtPubPlace.value,
-        txtPubName.value,
-        authors
-    );
-
-    txtOtherInfo.innerHTML = `<u><strong>In-Text Paraphrase:</strong></u><br><br>${book.getParaphrased()}
-    <br><br><u><strong>In-Text Quote:</strong></u><br><br>${book.getQuote()}`;
-    txtOutput.innerHTML = book.toString();
-});
 
 btnCopy.addEventListener("click", () => {
     if (txtOutput.value === null || txtOutput.value === "") {
